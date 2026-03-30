d9dbe01a3c6d9b7c06aa27bef8132c48254f4640
Cyben53 <xjamin271@gmail.com>
2026-03-30 23:08:41 +0100

    initial commit

---
+++ portfolio/js/script.js
@@ -0,0 +1,27 @@
+let tasks = [];
+
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    tasks.push(task);
+    input.value = "";
+
+    displayTasks();
+}
+
+function displayTasks() {
+    let list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    for (let i = 0; i < tasks.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = tasks[i];
+        list.appendChild(li);
+    }
+}
